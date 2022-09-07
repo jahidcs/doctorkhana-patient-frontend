76194a015832ec3d734266076b922065409dcc39
jahidcs <csq.jahid@gmail.com>
2022-09-07 22:41:34 +0600

    pages export components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Dashboard from './Dashboard/Dashboard'
+import Layout from './Layout/Layout'
+import LogReg from './LogReg/LogReg'
+import OtpAccess from './OtpAccess/OtpAccess'
+
+export { Layout, LogReg, OtpAccess, Dashboard }
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import DashboardPage from './DashboardPage'
+import LogRegPage from './LogRegPage'
+import OtpAccessPage from './OtpAccessPage'
+
+export { LogRegPage, OtpAccessPage, DashboardPage }
