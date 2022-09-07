4b6d4d2c8b92acf098ebe4dbea5b6c0857dbe61c
jahidcs <csq.jahid@gmail.com>
2022-09-07 22:43:22 +0600

    routing components and access_token state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from 'react-redux'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import { LogRegPage, OtpAccessPage, DashboardPage } from './pages'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { access_token } = useSelector((state) => state.auth)
+
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={!access_token ? <LogRegPage /> : <Navigate to="/dashboard" />} />
+                    <Route path="/otp" element={!access_token ? <OtpAccessPage /> : <Navigate to="/dashboard" />} />
+
+                    <Route path="dashboard" element={access_token ? <DashboardPage /> : <Navigate to="/" />} />
+
+                    <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
+                </Routes>
+            </Router>
+        </div>
+    )
 }
 
-export default App;
+export default App
