510d218b10a305c31bfd7204de26197a87d8ef0f
jahidcs <csq.jahid@gmail.com>
2022-09-11 08:41:09 +0600

    new routes: doctor profile, appointment details, appointment booking

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { useSelector } from 'react-redux'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
-import { LogRegPage, OtpAccessPage, DashboardPage } from './pages'
+import {
+    LogRegPage,
+    OtpAccessPage,
+    DashboardPage,
+    AppointmentMainPage,
+    DoctorProfilePage,
+    AppointmentBookPage,
+    AppointmentDetailsPage,
+} from './pages'
 
 function App() {
     const { access_token } = useSelector((state) => state.auth)
@@ -13,6 +21,10 @@ function App() {
                     <Route path="/otp" element={!access_token ? <OtpAccessPage /> : <Navigate to="/dashboard" />} />
 
                     <Route path="dashboard" element={access_token ? <DashboardPage /> : <Navigate to="/" />} />
+                    <Route path="appointment" element={access_token ? <AppointmentMainPage /> : <Navigate to="/" />} />
+                    <Route path="/doctor/:id" element={<DoctorProfilePage />} />
+                    <Route path="/book-appointment/:day" element={<AppointmentBookPage />} />
+                    <Route path="/appointment-details/:id" element={<AppointmentDetailsPage />} />
 
                     <Route path="*" element={<h1>Error 404 Page not found !!</h1>} />
                 </Routes>
